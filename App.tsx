a0b3d4af2d6023a1a62e933608041968b384beb0
luke-h1 <luke.howsam@yahoo.com>
2023-10-22 11:10:00 +0100

    feat(app): add deps

--- App.tsx
+++ App.tsx
@@ -1,20 +1,45 @@
 import { StatusBar } from 'expo-status-bar';
+import { useCallback, useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Entypo from '@expo/vector-icons/Entypo';
+import * as SplashScreen from 'expo-splash-screen';
+import * as Font from 'expo-font';
+import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
+  const [appIsReady, setAppIsReady] = useState(false);
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        // Pre-load fonts, make any API calls you need to do here
+        // Artificially delay for two seconds to simulate a slow loading
+        // experience. Please remove this if you copy and paste the code!
+        await new Promise(resolve => setTimeout(resolve, 2000));
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        // Tell the application to render
+        setAppIsReady(true);
+      }
+    }
+
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+   <NavigationContainer>
+
+   </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/screens/authentication/Login.tsx
@@ -0,0 +1,25 @@
+import { useRef } from 'react';
+import { WebView, WebViewNavigation } from 'react-native-webview';
+
+const LoginScreen = () => {
+  const ref = useRef<WebView | null>(null);
+  const twitchDarkTheme = `window.localStorage.setItem('twilight.theme', '1'); true;`;
+
+  return (
+    <WebView
+      source={{ uri: 'https://twitch.tv/login' }}
+      ref={ref}
+      onNavigationStateChange={() => {}}
+      onMessage={() => {}}
+      thirdPartyCookiesEnabled
+      injectedJavaScript={twitchDarkTheme}
+      cacheEnabled
+      sharedCookiesEnabled
+      style={{
+        flex: 1,
+      }}
+    />
+  );
+};
+
+export default LoginScreen;
