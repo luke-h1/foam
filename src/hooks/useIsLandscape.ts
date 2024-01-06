3b0b084b2184bffcc7dab43739716b89e8b21b06
luke-h1 <luke.howsam@yahoo.com>
2024-01-06 13:41:00 +0000

    refactor(app): refactor landscape utils

---
+++ src/hooks/useIsLandscape.ts
@@ -0,0 +1,21 @@
+import { useEffect, useState } from 'react';
+import { Dimensions } from 'react-native';
+
+const useIsLandscape = () => {
+  const [landscape, setLandscape] = useState(false);
+
+  Dimensions.addEventListener('change', () => {
+    setLandscape(Dimensions.get('window').width > 500);
+  });
+
+  useEffect(() => {
+    if (Dimensions.get('window').width > 500) {
+      setLandscape(true);
+    }
+  }, []);
+
+  return {
+    landscape,
+  };
+};
+export default useIsLandscape;
