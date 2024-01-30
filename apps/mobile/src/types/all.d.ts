a00137680e20b1d5046026d9ce4d3dbcd7246767
luke-h1 <luke.howsam@yahoo.com>
2024-01-30 23:15:18 +0000

    feat(app): convert to turborepo + pnpm workspace monorepo

---
+++ apps/mobile/src/types/all.d.ts
@@ -0,0 +1,40 @@
+import { config } from '../../tamagui.config';
+/* eslint-disable @typescript-eslint/no-explicit-any */
+declare module '*.svg' {
+  import React from 'react';
+  import { SvgProps } from 'react-native-svg';
+
+  const content: React.FC<SvgProps>;
+  export default content;
+}
+
+declare module '*.png' {
+  const value: any;
+  export = value;
+}
+
+declare module '*.jpg' {
+  const value: any;
+  export = value;
+}
+
+declare module '*.m3u' {
+  const value: any;
+  export = value;
+}
+
+type Conf = typeof config;
+
+declare module 'tamagui' {
+  // eslint-disable-next-line @typescript-eslint/no-empty-interface
+  interface TamaguiCustomConfig extends Conf {}
+}
+
+declare module 'react-devtools-core' {
+  export function connectToDevTools(options?: {
+    host?: string;
+    port?: number;
+    isAppActive?: () => boolean;
+    resolveRNStyle?: (style: any) => any;
+  }): void;
+}
