8913daa67f28aeb9a4dd614b55420445fcf429d3
luke-h1 <luke.howsam@yahoo.com>
2024-12-11 17:41:20 +0000

    --wip-- [skip ci]

--- src/components/ui/TextField.tsx
+++ src/components/ui/TextField.tsx
@@ -1,3 +1,4 @@
+import { colors, spacing, typography } from '@app/styles';
 import React, {
   ComponentType,
   forwardRef,
@@ -14,3 +15,234 @@ import {
   View,
   ViewStyle,
 } from 'react-native';
+import { Text, TextProps } from './Text';
+
+export interface TextFieldAccessoryProps {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  style: StyleProp<any>;
+  status: TextFieldProps['status'];
+  multiline: boolean;
+  editable: boolean;
+}
+
+export interface TextFieldProps extends Omit<TextInputProps, 'ref'> {
+  /**
+   * A style modifier for different input states.
+   */
+  status?: 'error' | 'disabled';
+  /**
+   * The label text to display
+   */
+  label?: TextProps['text'];
+  /**
+   * Pass any additional props directly to the label Text component.
+   */
+  LabelTextProps?: TextProps;
+  /**
+   * The helper text to display
+   */
+  helper?: TextProps['text'];
+  /**
+   * Pass any additional props directly to the helper Text component.
+   */
+  HelperTextProps?: TextProps;
+  /**
+   * The placeholder text to display
+   */
+  placeholder?: TextProps['text'];
+  /**
+   * Optional input style override.
+   */
+  style?: StyleProp<TextStyle>;
+  /**
+   * Style overrides for the container
+   */
+  containerStyle?: StyleProp<ViewStyle>;
+  /**
+   * Style overrides for the input wrapper
+   */
+  inputWrapperStyle?: StyleProp<ViewStyle>;
+  /**
+   * An optional component to render on the right side of the input.
+   * Example: `RightAccessory={(props) => <Icon icon="ladybug" containerStyle={props.style} color={props.editable ? colors.textDim : colors.text} />}`
+   * Note: It is a good idea to memoize this.
+   */
+  RightAccessory?: ComponentType<TextFieldAccessoryProps>;
+  /**
+   * An optional component to render on the left side of the input.
+   * Example: `LeftAccessory={(props) => <Icon icon="ladybug" containerStyle={props.style} color={props.editable ? colors.textDim : colors.text} />}`
+   * Note: It is a good idea to memoize this.
+   */
+  LeftAccessory?: ComponentType<TextFieldAccessoryProps>;
+}
+
+/**
+ * A component that allows for the entering and editing of text.
+ * @param {TextFieldProps} props - The props for the `TextField` component.
+ * @returns {JSX.Element} The rendered `TextField` component.
+ */
+
+export const TextField = forwardRef(function TextField(
+  props: TextFieldProps,
+  ref: Ref<TextInput>,
+) {
+  const {
+    label,
+    placeholder,
+    helper,
+    status,
+    RightAccessory,
+    LeftAccessory,
+    HelperTextProps,
+    LabelTextProps,
+    style: $inputStyleOverride,
+    containerStyle: $containerStyleOverride,
+    inputWrapperStyle: $inputWrapperStyleOverride,
+    // eslint-disable-next-line no-shadow
+    ...TextInputProps
+  } = props;
+
+  const input = useRef<TextInput>(null);
+
+  const disabled = TextInputProps.editable === false || status === 'disabled';
+
+  const placeholderContent = placeholder;
+
+  const $containerStyles = [$containerStyleOverride];
+
+  const $labelStyles = [$labelStyle, LabelTextProps?.style];
+
+  const $inputWrapperStyles: StyleProp<ViewStyle> = [
+    $inputWrapperStyle,
+    status === 'error' && { borderColor: colors.error },
+    TextInputProps.multiline && { minHeight: 112 },
+    LeftAccessory && { paddingStart: 0 },
+    RightAccessory && { paddingEnd: 0 },
+    $inputWrapperStyleOverride,
+  ];
+
+  const $inputStyles: StyleProp<TextStyle> = [
+    $inputStyle,
+    disabled && { color: colors.textDim },
+    TextInputProps.multiline && { height: 'auto' },
+    $inputStyleOverride,
+  ];
+
+  const $helperStyles = [
+    $helperStyle,
+    status === 'error' && { color: colors.error },
+    HelperTextProps?.style,
+  ];
+
+  function focusInput() {
+    if (disabled) {
+      return;
+    }
+    input.current?.focus();
+  }
+
+  useImperativeHandle(ref, () => input.current as TextInput);
+
+  return (
+    <TouchableOpacity
+      activeOpacity={1}
+      style={$containerStyles}
+      // eslint-disable-next-line react/jsx-no-bind
+      onPress={focusInput}
+      accessibilityState={{ disabled }}
+    >
+      {!!label && (
+        <Text
+          preset="formLabel"
+          text={label}
+          {...LabelTextProps}
+          style={$labelStyles}
+        />
+      )}
+      <View style={$inputWrapperStyles}>
+        {!!LeftAccessory && (
+          <LeftAccessory
+            style={$leftAccessoryStyle}
+            status={status}
+            editable={!disabled}
+            multiline={TextInputProps.multiline ?? false}
+          />
+        )}
+
+        <TextInput
+          ref={input}
+          underlineColorAndroid={colors.transparent}
+          textAlignVertical="top"
+          placeholder={placeholderContent}
+          placeholderTextColor={colors.textDim}
+          {...TextInputProps}
+          editable={!disabled}
+          style={$inputStyles}
+        />
+
+        {!!RightAccessory && (
+          <RightAccessory
+            style={$rightAccessoryStyle}
+            status={status}
+            editable={!disabled}
+            multiline={TextInputProps.multiline ?? false}
+          />
+        )}
+      </View>
+      {!!helper && (
+        <Text
+          preset="formHelper"
+          text={helper}
+          {...HelperTextProps}
+          style={$helperStyles}
+        />
+      )}
+    </TouchableOpacity>
+  );
+});
+
+const $labelStyle: TextStyle = {
+  marginBottom: spacing.extraSmall,
+};
+
+const $inputWrapperStyle: ViewStyle = {
+  flexDirection: 'row',
+  alignItems: 'flex-start',
+  borderWidth: 1,
+  borderRadius: 4,
+  backgroundColor: colors.palette.neutral200,
+  borderColor: colors.palette.neutral400,
+  overflow: 'hidden',
+};
+
+const $inputStyle: TextStyle = {
+  flex: 1,
+  alignSelf: 'stretch',
+  fontFamily: typography.primary.book,
+  color: colors.text,
+  fontSize: 16,
+  height: 24,
+  // https://github.com/facebook/react-native/issues/21720#issuecomment-532642093
+  paddingVertical: 0,
+  paddingHorizontal: 0,
+  marginVertical: spacing.extraSmall,
+  marginHorizontal: spacing.small,
+};
+
+const $helperStyle: TextStyle = {
+  marginTop: spacing.extraSmall,
+};
+
+const $rightAccessoryStyle: ViewStyle = {
+  marginEnd: spacing.extraSmall,
+  height: 40,
+  justifyContent: 'center',
+  alignItems: 'center',
+};
+
+const $leftAccessoryStyle: ViewStyle = {
+  marginStart: spacing.extraSmall,
+  height: 40,
+  justifyContent: 'center',
+  alignItems: 'center',
+};
