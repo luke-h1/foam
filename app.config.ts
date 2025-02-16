785e87885187b748f6d7a174f8d2fae5a3b12de6
luke-h1 <luke.howsam@yahoo.com>
2025-02-16 17:18:40 +0000

    chore: release 0.0.9

--- app.config.ts
+++ app.config.ts
@@ -56,7 +56,7 @@ const APP_VARIANT_CONFIG: Record<Variant, AppVariantConfig> = {
 
 const variant = (process.env.APP_VARIANT as Variant) || 'production';
 
-const VERSION = '0.0.8';
+const VERSION = '0.0.9';
 
 const appConfig = APP_VARIANT_CONFIG[variant];
 
