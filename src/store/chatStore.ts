553efe53d5025d2b0b4a2bf4d2d59434add4e36c
luke-h1 <luke.howsam@yahoo.com>
2025-04-06 11:36:36 +0100

    --wip-- [skip ci]

--- src/store/chatStore.ts
+++ src/store/chatStore.ts
@@ -1,8 +1,21 @@
+import { SanitisiedEmoteSet } from '@app/services';
 import { create } from 'zustand';
 import { createJSONStorage, persist } from 'zustand/middleware';
 import { zustandStorage } from './persist';
 
-interface ChatState {}
+interface ChatState {
+  // Twitch.tv emotes
+  twitchChannelEmotes: SanitisiedEmoteSet[];
+  twitchGlobalEmotes: SanitisiedEmoteSet[];
+
+  // 7TV emotes
+  sevenTvChannelEmotes: SanitisiedEmoteSet[];
+  sevenTvGlobalEmotes: SanitisiedEmoteSet[];
+
+  // FFZ emotes
+  ffzChannelEmotes: SanitisiedEmoteSet[];
+  ffzGlobalEmotes: SanitisiedEmoteSet[];
+}
 
 export const useChatStore = create(
   persist<ChatState>((set, get) => ({}), {
