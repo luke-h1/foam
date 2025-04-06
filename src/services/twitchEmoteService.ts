5ef384044fe5ea9178bf3103fdc7c8e889222519
luke-h1 <luke.howsam@yahoo.com>
2025-04-06 15:36:23 +0100

    --wip-- [skip ci]

--- src/services/twitchEmoteService.ts
+++ src/services/twitchEmoteService.ts
@@ -1,4 +1,5 @@
 import { ivrApi, twitchApi } from './api';
+import { SanitisiedEmoteSet } from './seventTvService';
 import { PaginatedList } from './twitchService';
 
 interface TwitchEmote {
@@ -13,6 +14,38 @@ interface TwitchEmote {
   theme_mode: ['light', 'dark'];
 }
 
+interface Emote {
+  id: string;
+  setID: string;
+  code: string;
+  type: 'SUBSCRIPTIONS' | 'FOLLOWER';
+  assetType: 'ANIMATED' | 'STATIC';
+  artist?: string | null; // Optional for local emotes
+}
+
+interface SubProduct {
+  displayName: string;
+  emoteSetID: string;
+
+  /**
+   * Tier 1 = 1000
+   * Tier 2 = 2000
+   * Tier 3 = 3000
+   */
+  tier: string;
+  emotes: Emote[];
+}
+
+interface LocalEmote {
+  id: string;
+  emotes: Emote[];
+}
+
+interface IvrEmoteResponse {
+  subProducts: SubProduct[];
+  bitEmotes: unknown[];
+  localEmotes: LocalEmote[];
+}
 export const twitchEmoteService = {
   getChannelEmotes: async (userId: string, cursor?: string) => {
     const result = await twitchApi.get<
@@ -28,13 +61,41 @@ export const twitchEmoteService = {
   },
 
   /**
-   * @todo -  move to our own API or do logic in app
+   * @todo - move to our own API or do logic in app
    */
-  getIvrChannelEmotes: async (channelId: string) => {
-    const result = await ivrApi.get('/twitch/emotes/channel', {
-      params: {
-        channel: channelId,
+  getIvrChannelEmotes: async (
+    channelId: string,
+  ): Promise<SanitisiedEmoteSet> => {
+    const result = await ivrApi.get<IvrEmoteResponse>(
+      '/twitch/emotes/channel',
+      {
+        params: {
+          channel: channelId,
+        },
       },
-    });
+    );
+
+    return result.subProducts.flatMap(
+      sub =>
+        sub.emotes.map(emote => ({
+          name: emote.code,
+          url: `https://static-cdn.jtvnw.net/emoticons/v2/${emote.id}/default/dark/3.0`,
+          emote_link: `https://static-cdn.jtvnw.net/emoticons/v2/${emote.id}/default/dark/3.0`,
+          site: 'Twitch Channel Emote',
+        })),
+      ...result.bitEmotes.map(emote => ({
+        name: emote.code,
+        url: `https://static-cdn.jtvnw.net/emoticons/v2/${emote.id}/default/dark/3.0`,
+        emote_link: `https://static-cdn.jtvnw.net/emoticons/v2/${emote.id}/default/dark/3.0`,
+        site: 'Twitch Channel Emote',
+      })),
+      ...result.localEmotes.flatMap((local) =>
+        local.emotes.map((emote) => ({
+          name: emote.code,
+          url: `https://static-cdn.jtvnw.net/emoticons/v2/${emote.id}/default/dark/3.0`,
+          emote_link: `https://static-cdn.jtvnw.net/emoticons/v2/${emote.id}/default/dark/3.0`,
+          site: "Twitch Channel Emote",
+        }))
+    );
   },
 } as const;
