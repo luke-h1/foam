abf121fc71b50761b7a68483f3de09e59a1b17a7
luke-h1 <luke.howsam@yahoo.com>
2025-07-09 21:27:46 +0100

    --wip-- [skip ci] - emoji picker

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,17 +1,3 @@
-// commitlint.config.js
-
-const scopes = [
-  'app',
-  'infrastructure',
-  'ui',
-  'ci',
-  'docs',
-  'test',
-  'documentation',
-].map(name => ({
-  name,
-}));
-
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
@@ -19,42 +5,26 @@ module.exports = {
       2,
       'always',
       [
-        'feat',
-        'fix',
-        'style',
-        'refactor',
-        'perf',
+        'app',
+        'ci',
         'test',
-        'chore',
-        'revert',
-        'release',
+        'docs',
+        'infrastructure',
+        'chat',
+        'twitch',
+        'seventv',
+        'bttv',
+        'ffz',
+        'stream',
+        'auth',
       ],
     ],
-    'subject-case': [0], // Allow any case for the subject
   },
-  types: [
-    { value: 'feat', name: 'feat:\tAdding a new feature' },
-    { value: 'fix', name: 'fix:\tFixing a bug' },
-    { value: 'style', name: 'style: Add or update styles' },
-    {
-      value: 'refactor',
-      name: 'refactor:\tCode change that neither fixes a bug nor adds a feature',
-    },
-    { value: 'perf', name: 'perf:\tCode change that improves performance' },
-    { value: 'test', name: 'test:\tAdding test cases / adds missing tests' },
-    {
-      value: 'chore',
-      name: 'chore:\tChanges to the build process or auxiliary tools and libraries such as documentation generation',
-    },
-    { value: 'revert', name: 'revert:\tRevert to a commit' },
-    { value: 'release', name: 'release:\tRelease a new version (e.g., 0.0.2)' },
-  ],
-  scopes,
   scopeOverrides: {
     chore: [...scopes, { name: 'release' }],
   },
   allowCustomScopes: true,
-  allowBreakingChanges: ['feat', 'fix', 'perf', 'refactor'],
+  allowBreakingChanges: ['feat', 'fix', 'perf', 'refactor', 'hotfix'],
   subjectLimit: 100,
   skipQuestions: ['body'],
 };
