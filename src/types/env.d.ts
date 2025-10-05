5dcc7fe8013b2131a9f0b4476cc59c4496ceff3b
luke-h1 <luke.howsam54@gmail.com>
2025-10-05 14:36:32 +0100

    chore(app): add expo-atlas

--- src/types/env.d.ts
+++ src/types/env.d.ts
@@ -5,9 +5,6 @@ declare global {
       TWITCH_CLIENT_SECRET: string;
       AUTH_PROXY_API_BASE_URL: string;
       AUTH_PROXY_API_KEY: string;
-
-      NEW_RELIC_IOS_APP_TOKEN: string;
-      NEW_RELIC_ANDROID_APP_TOKEN: string;
     }
   }
 }
