c2ba2543a8045837084ac02d52cb6807b2d102df
Luke Howsam <55030296+luke-h1@users.noreply.github.com>
2025-12-22 05:54:22 -0800

    chore(sentry): adjust sentry rates (#419)

--- src/services/sentry-service.ts
+++ src/services/sentry-service.ts
@@ -4,6 +4,11 @@ const navigationIntegration = Sentry.reactNavigationIntegration({
   enableTimeToInitialDisplay: true,
 });
 
+/**
+ * Adjust once we have users
+ */
+const sampleRate = 1;
+
 Sentry.init({
   dsn: 'https://c66140f9c8c6c72a91e15582f3086de5@o536134.ingest.us.sentry.io/4508831072780288',
   attachScreenshot: true,
@@ -13,6 +18,15 @@ Sentry.init({
   spotlight: __DEV__,
   appHangTimeoutInterval: 1,
   enabled: !__DEV__,
+  profilesSampleRate: sampleRate,
+  tracesSampleRate: sampleRate,
+
+  /**
+   * Replays are expensive, let's disable them
+   */
+  replaysOnErrorSampleRate: 0,
+  replaysSessionSampleRate: 0,
+
   enableAutoPerformanceTracing: true,
   enableUserInteractionTracing: true,
   environment: process.env.APP_VARIANT as string,
