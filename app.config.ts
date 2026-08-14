169bd2dffb854b2bb3bdd40ad83f6716752d2685
luke-h1 <luke.howsam@yahoo.com>
2026-08-14 23:40:19 +0100

    chore(release): 1.0.8

--- app.config.ts
+++ app.config.ts
@@ -78,7 +78,7 @@ export const VARIANT_CONFIG: Record<Variant, AppVariantConfig> = {
 const variant =
   (process.env.EXPO_PUBLIC_APP_VARIANT as Variant) || 'development';
 
-const VERSION = '1.0.7';
+const VERSION = '1.0.8';
 
 const appConfig = VARIANT_CONFIG[variant];
 const twitchClientId = process.env.EXPO_PUBLIC_TWITCH_CLIENT_ID;
